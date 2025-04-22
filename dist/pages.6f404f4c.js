d1fc356383e2bd0c562a1bbcba04b29aff01471b
umavanmoriel <umeda19971102@gmail.com>
2025-04-22 16:08:51 +0200

    Lade till funktioner för att hämta info om hundar från api

--- dist/pages.6f404f4c.js
+++ dist/pages.6f404f4c.js
@@ -667,6 +667,52 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"lFEsO":[function(require,module,exports,__globalThis) {
+// Initialisering när sidan laddas om
+window.onload = init;
+// Funktion för att initialisera applikationen och hämta information
+function init() {
+    processData();
+}
+//Hämta kurser
+async function getCoursesInfo() {
+    try {
+        const response = await fetch('https://dogapi.dog/api/v2/breeds?format=json');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Fetch error:', error);
+        throw error;
+    }
+}
+// Användning av den asynkrona funktionen
+async function processData() {
+    try {
+        const result = await getCoursesInfo();
+        console.log('Received data:', result);
+        coursesInfoDisplay(result);
+    } catch (error) {
+        console.error('Error processing data:', error);
+    }
+}
+// Visa information för kurser
+function coursesInfoDisplay(data) {
+    const coursesListEl = document.getElementById('adoption-container');
+    // Rensa tidigare innehåll
+    coursesListEl.innerHTML = '';
+    // Loopa genom och skapa nya list element
+    data.data.forEach((dog)=>{
+        const newRowEl = document.createElement('section');
+        const courseCodeEl = document.createElement('h2');
+        const courseCodeTextEl = document.createTextNode(dog.attributes.name);
+        courseCodeEl.appendChild(courseCodeTextEl);
+        newRowEl.appendChild(courseCodeEl);
+        const courseNameEl = document.createElement('p');
+        const courseNameTextEl = document.createTextNode(dog.attributes.description);
+        courseNameEl.appendChild(courseNameTextEl);
+        newRowEl.appendChild(courseNameEl);
+        coursesListEl.appendChild(newRowEl);
+    });
+}
 
 },{}]},["jSNv4","lFEsO"], "lFEsO", "parcelRequire170a", {})
 

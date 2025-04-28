785e815d5ecea0454e47595caf9ca46f9a5038a6
umavanmoriel <umeda19971102@gmail.com>
2025-04-28 21:42:07 +0200

    Lade till function i dogimages.js för att fetcha bilder

---
+++ src/functions/dogimages.js
@@ -0,0 +1,43 @@
+// Initialisering när sidan laddas om
+window.onload = init;
+
+// Funktion för att initialisera applikationen och hämta information
+function init() {
+    processImageData();
+}
+
+//Hämta information
+async function getImagesInfo() {
+    try {
+        const imageResponse = await fetch(`https://dog.ceo/api/breeds/image/random/20`);
+        const imageData = await imageResponse.json();
+        return imageData;
+    } catch (error) {
+        console.error('Fetch error:', error);
+        throw error;
+    }
+};
+
+// Användning av den asynkrona funktionen
+async function processImageData() {
+    try {
+        const imgResult = await getImagesInfo();
+        console.log('Received data:', imgResult);
+        imagesDisplay(imgResult);
+    } catch (error) {
+        console.error('Error processing data:', error);
+    }
+}
+
+// Visa information för hundraser
+function imagesDisplay(data) {
+    const imageSectionEl = document.getElementById('images-container'); 
+    // Rensa tidigare innehåll
+    imageSectionEl.innerHTML = ''; 
+    data.message.forEach( image=> {
+        const imgEl = document.createElement('img');
+        imgEl.src = image;
+        imgEl.width = 200;
+        imageSectionEl.appendChild(imgEl);
+    });
+}

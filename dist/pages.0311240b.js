a5fb2219e9ea632f27f1e9d161d929b1257ef035
umavanmoriel <umeda19971102@gmail.com>
2025-07-25 19:59:50 +0200

    kör kommandot npm run build

---
+++ dist/pages.0311240b.js
@@ -0,0 +1,2 @@
+!function(){let e=document.getElementById("menu"),t=document.getElementById("dropdown-menu");e.addEventListener("click",()=>{t.classList.toggle("show")})}();
+//# sourceMappingURL=pages.0311240b.js.map
---
+++ dist/pages.90b53795.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("menu"),t=document.getElementById("dropdown-menu");e.addEventListener("click",()=>{t.classList.toggle("show")});
+//# sourceMappingURL=pages.90b53795.js.map
